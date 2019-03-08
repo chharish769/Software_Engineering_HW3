cbaca7192b4d42963de545db505779491260de6d
chharish769 <chharish769@gmail.com>
2019-03-08 07:55:43 +0000

    part 1

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -1,8 +1,14 @@
 
-Given /the following movies exist/ do |movies_table|
-  movies_table.hashes.each do |movie|
-    Movie.create movie
-  end
+#Given /the following movies exist/ do |movies_table|
+#  movies_table.hashes.each do |movie|
+#    Movie.create movie
+#  end
+#end
+
+Given /the following movies exist:$/ do |movies_table|
+    movies_table.hashes.each do |hash|
+        Movie.create!(title: hash['title'], rating: hash['rating'], release_date: hash['release_date'], director: hash['director'])
+    end
 end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
